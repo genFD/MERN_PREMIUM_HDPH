eaf03972c777673b5d2e626f190dd2b71b9ce9a2
hermkan <hermann.kanga.dev@gmail.com>
2022-01-12 12:55:13 -0500

    established connection with db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/data.js';
 
 dotenv.config();
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
